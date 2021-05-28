7058d86a4521bdcbf5cd01c63e0fb1541aa10641
ZeroMomentum <zero@envs.net>
2021-05-28 15:02:20 -0500

    the commit thing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import os
 
 app = Flask(__name__)
@@ -8,6 +8,9 @@ app = Flask(__name__)
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
+@app.route("/d")
+def d():
+    return render_template("<h1> a </h1>")
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
