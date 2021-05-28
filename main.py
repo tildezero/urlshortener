41bd08dd458d2f529fa6ce1caf418f0a20c494ea
ZeroMomentum <zero@envs.net>
2021-05-28 15:30:59 -0500

    the commit thing

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template 
 import os
+from secrets import token_urlsafe
+import redis
 
 app = Flask(__name__)
+db = redis.Redis(host = os.getenv("REDISHOST"), port=os.getenv("REDISPORT"), password=os.getenv("REDISPASSWORD"))
+
+@app.route('/') 
+def index(): 
+    return render_template("form.html") 
+
+@app.route("/link")
+def link():
+    pass
 
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
-@app.route("/d")
-def d():
-    return render_template("<h1> a </h1>")
+@app.route('/shorten')
+def shorten():
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
